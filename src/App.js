90355158fb8f75cd5e79b230340a185f032eb6f7
Priyanka Yadav <ydpriyanka01@gmail.com>
2021-07-11 23:06:25 +0530

    deleted some files

--- src/App.js
+++ src/App.js
@@ -1,63 +1,72 @@
-import React ,{useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Recipe from './component/Recipe';
 import Axios from 'axios';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
+// imported axios to fetch data from api
+//imported uuidv4 to use as a key  
 
 
 function App() {
-  const[query,setQuery]= useState("");
-  const [recipes,setRecipes]=useState([]);
+  //we are using here functional component
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  //we are maintaining state through react hooks
 
 
 
+  const URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
 
-  const URL=`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+  //this is url of api themealdb
 
-  const getData= async()=>{
-    try{
-    const result=await Axios.get(URL);
-   /*  if(result.data.meals===null) */
-    setRecipes(result.data.meals)
-   
-    console.log(result.data);
-    setQuery("");
-    }catch(error){
+  const getData = async () => {
+
+    //we are using try and catch method to handle errors
+    //we are using async to catch promises
+    try {
+      const result = await Axios.get(URL);
+     setRecipes(result.data.meals)
+
+      console.log(result.data);
+      setQuery("");
+    } catch (error) {
       console.error();
-      
+
     }
-  
+
 
   };
-  const onSubmit=(e)=>{
-     e.preventDefault();
-     getData();
+  const onSubmit = (e) => {
+    e.preventDefault();
+    //it prevents auto-reloads
+    getData();
   }
-  const onChange =(e)=>{
-   setQuery(e.target.value);
+  const onChange = (e) => {
+    setQuery(e.target.value);
   }
-  
+
   return (
     <div className="Heading">
-     <style>{'body { background-color: rgb(173, 230, 227); }'}</style>
+      <style>{'body { background-color: rgb(173, 230, 227); }'}</style>
       <form onSubmit={onSubmit}>
         <div className="Header">
-   <h1 >Recipe Finder</h1> <br/></div>
-    <input type="text" placeholder="Enter the name of Dish " autoComplete="off" onChange={onChange} value={query}/>
-    
-    <input type="submit" value="Get Recipe" />
+          <h1 >Recipe Finder</h1> <br /></div>
+        <input type="text" placeholder="Enter the name of Dish " autoComplete="off" onChange={onChange} value={query} />
+
+        <input type="submit" value="Get Recipe" />
 
 
-   </form>
-   <h2>{recipes!==null?"Type a Dish Name to Search for its Ingredients":"no data found"}</h2>
-
-   <div className="recipes">
-     {recipes!==null && recipes?.map(recipe=><Recipe key={uuidv4} recipe={recipe}/>)} 
-   </div>
+      </form>
+      <h2>{recipes !== null ? "Type a Dish Name to Search for its Ingredients" : "No Data has been received"}</h2>
+     { /* we are using here ternary operator to check the condition if data entered is present or not and print accordingly */}
+      <div className="recipes">
+        {recipes !== null && recipes?.map(recipe => <Recipe key={uuidv4} recipe={recipe} />)}
+      </div>
     </div>
-  
+
   );
-  
-  }
+
+}
 
 export default App;
--- src/component/Recipe.js
+++ src/component/Recipe.js
@@ -1,21 +1,22 @@
-import React ,{useState}from "react";
+import React, { useState } from "react";
 import Ingredient from "./Ingredient";
 import "./Recipe.css";
-import Axios from 'axios';
+//import Axios from 'axios';
 import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
 
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Favorite from '@material-ui/icons/Favorite';
+//import Favorite from '@material-ui/icons/Favorite';
 import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 
 
 
 
 const Recipe = (recipe) => {
-	const { strMeal, strMealThumb, strCategory, strArea, strInstructions ,strSource} =
+	const { strMeal, strMealThumb, strCategory, strArea, strInstructions, strSource } =
 		recipe.recipe;
-        
+		//here we are intializing variables which are given in api 
+
 
 	console.log(recipe);
 
@@ -26,37 +27,37 @@ const Recipe = (recipe) => {
 		ingredientsKeyNames[a - 1] = `strIngredient${a}`;
 		quantityKeyNames[a - 1] = `strMeasure${a}`;
 	}
-    
 
 
-  
+
+
 
 	return (
 		<div className="recipe">
-            <div className="header">
-			{/* <h1 >{strMeal}<span className="like"><button aria-hidden="true" className="like-btn"><FavoriteBorderOutlinedIcon/></button>
-						 </span></h1> */}
-            <h1><a href={strSource} target="_blank" rel="noopenernoreferred">{strMeal}<span className="like">
-            <FormControlLabel
-        control={<Checkbox icon={<FavoriteBorderOutlinedIcon fontSize="large" />} 
-                  checkedIcon={<FavoriteBorder fontSize="large" />}
-          name="checkedH" />}
-      
-      />
-                </span></a></h1>
-            </div>
+			<div className="header">
+
+				<h1><a href={strSource} target="_blank" rel="noopenernoreferred">{strMeal}<span className="like">
+					<FormControlLabel
+						control={<Checkbox icon={<FavoriteBorderOutlinedIcon fontSize="large" />}
+							checkedIcon={<FavoriteBorder fontSize="large" />}
+							name="checkedH" />}
+
+					/>
+				</span></a></h1>
+			</div>
+			{/*above code is for heading purpose and we are using href for redirecting to main source  and we are using material ui material icons i.e."favoritBorderOutline icon" to display the like icon  */}
 			<img src={strMealThumb} alt={strMealThumb} />
 			<div className="catarea">
-				<h3>Category of meal: {strCategory} <br/> area of the meal: {strArea}</h3>
+				<h3><em >Category of Meal: </em>{strCategory}<br /> <em>Area of the Meal:</em> {strArea}</h3>
 			</div>
-            <h3 className="Ingre-head">Ingredients</h3>
-            <div class="Ingre">
-			<Ingredient
-				recipe={recipe}
-				ingredientsKeyNames={ingredientsKeyNames}
-				quantityKeyNames={quantityKeyNames}
-			/></div>
-            <h3 className="recipe-head">Recipes </h3>
+			<h3 className="Ingre-head">Ingredients</h3>
+			<div className="Ingre">
+				<Ingredient
+					recipe={recipe}
+					ingredientsKeyNames={ingredientsKeyNames}
+					quantityKeyNames={quantityKeyNames}
+				/></div>
+			<h3 className="recipe-head">Recipes </h3>
 			<div className="inst">
 				<h3>{strInstructions}</h3>
 			</div>
